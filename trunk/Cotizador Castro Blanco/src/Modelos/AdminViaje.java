2fa8438138f6f5a9da3b6a0d555b5ba1b6c55ac8
tomas.cereminati@gmail.com <tomas.cereminati@gmail.com@6efc57aa-df9b-11de-b2e4-dbb720ae924e>
2009-12-15 18:37:35 +0000

    POR DIOS QUE ESTE SI ANDE !!!

--- trunk/Cotizador Castro Blanco/src/Modelos/AdminViaje.java
+++ trunk/Cotizador Castro Blanco/src/Modelos/AdminViaje.java
@@ -2,18 +2,26 @@
 package Modelos;
 
 import DAO.CamionDAO;
+import DAO.ViajeDAO;
 import Entidades.Camion;
+import java.sql.Date;
 import java.util.List;
 
 public class AdminViaje
 {
     private CamionDAO camionDAO ;
+    private ViajeDAO viajeDAO ;
 
     public AdminViaje (){
         this.camionDAO = new CamionDAO () ;
+        this.viajeDAO = new ViajeDAO () ;
     }
 
     public List<Camion> obtenerCamiones (){
         return camionDAO.obtenerTodos() ;
+    }
+
+    public boolean verificarCamionDisponibleParaFechas (String patente , Date fechaSalida){
+        return this.viajeDAO.verificarCamionDisponibleParaFechas(patente, fechaSalida) ;
     }
 }
